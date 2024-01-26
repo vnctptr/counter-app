a1bb7faa278e7cd6c9e62f20391f33692d473f89
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-25 17:40:47 -0800

    Tweak the spacing on the sheet

--- CounterApp/CounterApp/Views/CounterViews.swift
+++ CounterApp/CounterApp/Views/CounterViews.swift
@@ -39,11 +39,11 @@ struct CounterDetailSheetView: View {
             Text(counter.name)
                 .font(.system(size: 20))
                 .padding(.leading, 10)
-            HStack (spacing: 15){
+            HStack (spacing: 40){
                 CounterButton(imageName: "minus.circle.fill")
                 CounterButton(imageName: "gobackward")
                 CounterButton(imageName: "plus.circle.fill")
-            }.padding()
+            }.padding(.vertical, 20)
         }
     }
 }
