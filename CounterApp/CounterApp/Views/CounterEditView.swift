e339e55ca042367b5a33c7d358a11782714998ce
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-27 19:44:43 -0800

    Add Counter edit page

--- CounterApp/CounterApp/Views/CounterEditView.swift
+++ CounterApp/CounterApp/Views/CounterEditView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct CounterEditView: View {
+    @State private var count = 0
+    @State private var counterName = "Counter"
+    @State private var selectedColor = Color.blue
+    @State private var itemTitle: String = ""
+    
     
     var body: some View {
-        
-        Text("Settings")
-    }
-    
+
+        Color.gray.opacity(0.1)
+            .edgesIgnoringSafeArea(.all)
+            .overlay(
+                VStack {
+                    Text("Edit counter")
+                        .font(.headline)
+                        .padding(.top, 25)
+                    VStack (spacing: 20){
+                        TextField("Counter title", text: $itemTitle)
+                        ColorPicker("Select Color", selection: $selectedColor)
+                        HStack {
+                            Text("Archive")
+                            Spacer()
+                        }
+                        HStack {
+                            Text("Delete").foregroundColor(.red)
+                            Spacer()
+                        }
+                    }.padding(20)
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color.white)
+                        ).padding(20)
+                Spacer()
+                }
+
+            )   }
 }
