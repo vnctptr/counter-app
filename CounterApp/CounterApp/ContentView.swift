f72ed044d0617643966d34e0e4e8a9b269b8328e
Vincent Potrykus <vapotrykus@gmail.com>
2024-01-31 18:45:34 -0800

    Change background colour for dark mode

--- CounterApp/CounterApp/ContentView.swift
+++ CounterApp/CounterApp/ContentView.swift
@@ -9,57 +9,35 @@ import SwiftUI
 import SwiftData
 
 
-
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         NavigationSplitView {
             VStack {
                 CounterList()
                 Spacer()
-            }
-            .toolbar {
-                //                TODO: Uncomment this when filters are added
-                //                ToolbarItem(placement: .navigationBarLeading) {
-                //                    Menu {
-                //                        Button {
-                //                            print("button pressed")
-                //                        } label: {
-                //                            Text("Category 1")
-                //                        }
-                //                    } label: {
-                //                        HStack {
-                //                            Text("All counters").foregroundColor(.black)
-                //                            Image(systemName: "chevron.down")
-                //                        }
-                //                        .foregroundColor(.black)
-                //                    }
-                //                }
-                
-                ToolbarItem {
-                    NavigationLink(destination: CounterCreateView()) {
-                        Image(systemName: "plus").foregroundColor(Color.primary)
+            }.background(colorScheme == .dark ? Color(UIColor(red: 14.00 / 255.0, green: 14.00 / 255.0, blue: 15.00 / 255.0, alpha: 1.00)) : Color.white)
+                .toolbar {
+                    ToolbarItem {
+                        NavigationLink(destination: CounterCreateView()) {
+                            Image(systemName: "plus").foregroundColor(Color.primary)
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: SettingsView()) {
+                            Image(systemName: "gearshape.fill").foregroundColor(Color.primary)
+                        }
                     }
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: SettingsView()) {
-                        Image(systemName: "gearshape.fill").foregroundColor(Color.primary)
-                    }
-                    
-                }
-            }
         } detail: {
             Text("Select an item")
         }
     }
-    
-
-
 }
-
 
 #Preview {
     ContentView()
-        
+    
 }
