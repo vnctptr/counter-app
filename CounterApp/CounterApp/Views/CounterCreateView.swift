a5e0a7b4734bc2fba7e045874a366eca727e0bcd
Vincent Potrykus <vapotrykus@gmail.com>
2024-02-16 21:29:08 -0800

    Add a function to convert Color to hex

--- CounterApp/CounterApp/Views/CounterCreateView.swift
+++ CounterApp/CounterApp/Views/CounterCreateView.swift
@@ -62,3 +62,13 @@ struct CounterCreateView: View {
     }
         
 }
+
+func colorToHexString(color: UIColor) -> String {
+    let components = color.cgColor.components
+    let r: CGFloat = components?[0] ?? 0.0
+    let g: CGFloat = components?[1] ?? 0.0
+    let b: CGFloat = components?[2] ?? 0.0
+
+    let hexString = String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+    return hexString
+ }
