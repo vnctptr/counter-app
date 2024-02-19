d8b380567423b2d09b8125781cbe916d126f9d6d
Vincent Potrykus <vapotrykus@gmail.com>
2024-02-18 20:27:01 -0800

    Add a function to convert hex to Color

--- CounterApp/CounterApp/Views/CounterCreateView.swift
+++ CounterApp/CounterApp/Views/CounterCreateView.swift
@@ -72,3 +72,32 @@ func colorToHexString(color: UIColor) -> String {
     let hexString = String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
     return hexString
  }
+
+func hexStringToColor(hexString: String) -> UIColor {
+    var colorString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+    colorString = colorString.replacingOccurrences(of: "#", with: "").uppercased()
+
+    let alpha: CGFloat = 1.0
+    let red: CGFloat = colorComponentFrom(colorString: colorString, start: 0, length: 2)
+    let green: CGFloat = colorComponentFrom(colorString: colorString, start: 2, length: 2)
+    let blue: CGFloat = colorComponentFrom(colorString: colorString, start: 4, length: 2)
+
+    let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    return color
+}
+
+func colorComponentFrom(colorString: String, start: Int, length: Int) -> CGFloat {
+
+    let startIndex = colorString.index(colorString.startIndex, offsetBy: start)
+    let endIndex = colorString.index(startIndex, offsetBy: length)
+    let subString = colorString[startIndex..<endIndex]
+    let fullHexString = length == 2 ? subString : "\(subString)\(subString)"
+    var hexComponent: UInt32 = 0
+
+    guard Scanner(string: String(fullHexString)).scanHexInt32(&hexComponent) else {
+        return 0
+    }
+    let hexFloat: CGFloat = CGFloat(hexComponent)
+    let floatValue: CGFloat = CGFloat(hexFloat / 255.0)
+    return floatValue
+}
